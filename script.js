7d968fceb9b998980cfb9c686daa8cd363092568
BoJanggles77 <jorge.saro@yahoo.com>
2017-05-07 12:50:43 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+if (!localStorage.gridData) {
+	var data = {};
+	for (var i = 0; i < 20; i++) {
+		var dataEl = {
+			name: '',
+			location: "Fremont Locker #" + i.toString(),
+			available: true
+		};
+		data[i] = JSON.stringify(dataEl);
+		localStorage.setItem("gridData", JSON.stringify(data));
+	}
+}
+
+for (var i = 0; i < 20; i++) {
+	var element = document.getElementById(i.toString());
+	var data = JSON.parse(localStorage.getItem("gridData"));
+	var dataEl = JSON.parse(data[i]);
+	if (!dataEl.available) {
+		element.style.backgroundColor = 'red';
+	} else {
+		element.style.backgroundColor = 'green';
+	}
+}
+
 function toggleView(){
 	var x = document.getElementById('snoop');
 	if(x.style.display=== 'none'){
@@ -8,25 +32,18 @@ function toggleView(){
 	}
 }
 
+function changeBackground(color) {
+   document.body.style.background = color;
+}
+
 function reappearRent(){
 	var x = document.getElementById("option1");
 	x.style.display = "inline";
 }
 
-function disappearRent(){
-	var x = document.getElementById("option1");
-	x.style.display = "none";
-	
-}
-
 function reappearCheckout(){
 	var x = document.getElementById("option2");
 	x.style.display = "inline";
-}
-
-function disappearCheckOut(){
-	var x = document.getElementById("option2");
-	x.style.display = "none";
 }
 
 function appearTable(){
@@ -42,36 +59,53 @@ function disappearTable(){
 var button = document.getElementById('click');
 button.addEventListener("click", function() {
 	toggleView();
-	reappearRent();
-	reappearCheckout();
 	disappearTable();
+	changeBackground("#C3C2CA");
+	bool = true;
+	
 });
 
 var button1 = document.getElementById("option1");
 button1.addEventListener("click", function(){
-	disappearRent();
-	disappearCheckOut();
 	appearTable();
+	changeBackground("#93F096");
+	bool = false;
 });
 
 var button2 = document.getElementById("option2");
 button2.addEventListener("click", function(){
-	disappearRent();
-	disappearCheckOut();
 	appearTable();
+	changeBackground("#FF7070");
+	bool = true;
 });
 
 function changeColor(e){
+	var name = document.getElementById('textbox').value;
     var x = e.srcElement;
     console.log(x);
     if (x.id !== 'locker') {
-    	if (x.style.backgroundColor === 'red') {
+		var data = JSON.parse(localStorage.getItem("gridData"));
+		var dataEl = JSON.parse(data[parseInt(x.id)]);
+    	var nameDisplay = document.querySelector("[id='" + x.id + "'] span");
+    	console.log(dataEl.available);
+    	console.log(x.style.backgroundColor);
+    	if (bool === true &&  x.style.backgroundColor === 'red' && name === dataEl.name) {
 	    	x.style.backgroundColor = 'green';
-	    } else {
+	    	dataEl.available = true;
+	    	dataEl.name = '';
+	    	nameDisplay.innerHTML = '';
+	    } else if (bool === false &&  x.style.backgroundColor == 'green') {
 	    	x.style.backgroundColor = 'red';
+	    	dataEl.available = false;
+	    	dataEl.name = name;
+	    	nameDisplay.innerHTML = name;
 	    }
+	    data[parseInt(x.id)] = JSON.stringify(dataEl);
+	    localStorage.setItem("gridData", JSON.stringify(data));
     }
 }
 
 var locker = document.getElementById("locker");
 locker.addEventListener("click",changeColor);
+
+var bool = true;
